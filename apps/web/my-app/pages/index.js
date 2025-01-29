dc772683974d2447c69fd65fef96581df8ed68ed
rashidpbi <rashidpbi786@gmail.com>
2025-01-29 11:42:04 +0530

    feat: implemented sample package under turbo
    implemented a simple package that contains an add function under packages in the root folder

--- apps/web/my-app/pages/index.js
+++ apps/web/my-app/pages/index.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
-
+import {add} from '@repo/math/add'
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -12,6 +12,7 @@ const geistMono = Geist_Mono({
 });
 
 export default function Home() {
+  let num = add(2,4);
   return (
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
@@ -29,7 +30,7 @@ export default function Home() {
           <li className="mb-2">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              pages/index.js
+              pages/index.js result of add {num}
             </code>
             .
           </li>
