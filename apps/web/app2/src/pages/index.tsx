b44b66367a598138782260ed4402443484faac0c
rashidpbi <rashidpbi786@gmail.com>
2025-02-09 01:12:11 +0530

    feat:configuration added to travel-app project

--- apps/web/app2/src/pages/index.tsx
+++ apps/web/app2/src/pages/index.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import RootLayout from "./layout";
 import Home from "../components/Home";
-import Content from '../components/Content'
+import Content from "../components/Content";
 export default function Page() {
   return (
     <div>
-<Home bgImg={"bg-hero"}><Content/></Home>
+      <Home bgImg={"bg-hero"}>
+        <Content />
+      </Home>
     </div>
   );
 }
-Page.getLayout = function getLayout(page) {
+Page.getLayout = function getLayout(page:any) {
   return <RootLayout>{page}</RootLayout>;
 };
